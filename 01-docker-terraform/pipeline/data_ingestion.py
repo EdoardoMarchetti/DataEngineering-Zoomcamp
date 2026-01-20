13e23e48911a66596556a0563b968a86000efd64
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2026-01-20 17:13:21 +0100

    parametri click creati

---
+++ 01-docker-terraform/pipeline/data_ingestion.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import click
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+
+def ingest_data(
+        url: str,
+        engine,
+        target_table: str,
+        chunksize: int = 100000,
+) -> pd.DataFrame:
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
+
+    first_chunk = next(df_iter)
+
+    first_chunk.head(0).to_sql(
+        name=target_table,
+        con=engine,
+        if_exists="replace"
+    )
+
+    print(f"Table {target_table} created")
+
+    first_chunk.to_sql(
+        name=target_table,
+        con=engine,
+        if_exists="append"
+    )
+
+    print(f"Inserted first chunk: {len(first_chunk)}")
+
+    for df_chunk in tqdm(df_iter):
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append"
+        )
+        print(f"Inserted chunk: {len(df_chunk)}")
+
+    print(f'done ingesting to {target_table}')
+
+@click.command()
+@click.option('--year', required=True, type=int, help='Year of the data (e.g., 2021)')
+@click.option('--month', required=True, type=int, help='Month of the data (1-12)')
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for data ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--url-prefix', default='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow', help='URL prefix for data files')
+def main(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, chunksize, target_table, url_prefix):
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
+
+    ingest_data(
+        url=url,
+        engine=engine,
+        target_table=target_table,
+        chunksize=chunksize
+    )
+
+if __name__ == '__main__':
+    main()
